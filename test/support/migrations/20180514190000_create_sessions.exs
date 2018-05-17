1393c45428ca4803b561db76de16ad7ab3552e67
YeFFreY <johan.mslms@outlook.com>
2018-05-17 16:33:33 +0200

    session table

--- test/support/migrations/20180514190000_create_sessions.exs
+++ test/support/migrations/20180514190000_create_sessions.exs
@@ -4,7 +4,9 @@ defmodule TestMigration do
   def change do
     create table(:plug_sessions) do
       add :sid, :string, null: false
-      add :data, :string, null: false
+      add :data, :map, null: false
+      add :last_modified, :naive_datetime, null: false, default: fragment("now()")
+      add :created, :naive_datetime, null: false, default: fragment("now()")
     end
 
     create unique_index(:plug_sessions, [:sid])
